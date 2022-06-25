0275c1e4748838b7e8d34c0185da5dfe2c2514e2
Nyjia Williams <nyjiawilliams@gmail.com>
2022-06-25 15:21:53 -0500

    added tracks from remote.local streams to peer connection

--- main.js
+++ main.js
@@ -18,18 +18,41 @@ let init = async () => {
   //set localStreat to the mediadevice prefered by the user
   localStream = await navigator.mediaDevices.getUserMedia({audio:false, video:true});
 
-  //set remoteStream to using peer2peer connection
-  remoteStream = new MediaStream()
 
   //set streamms to specified DOM elements
   document.getElementById('user-1').srcObject = localStream
-  document.getElementById('user-2').srcObject = remoteStream
 }
 
 let createOffer = async () => {
   //create the peer connection object with the server we created for negotiations
   peerConnection = new RTCPeerConnection(servers)
+
+  //set remoteStream to using peer2peer connection
+  remoteStream = new MediaStream()
+  document.getElementById('user-2').srcObject = remoteStream
+
+  //add local stream audioand video tracks to peer connection
+  localStream.getTrack().forEach((track) => {
+    peerConnection.addTrack(track, localStream)
+  });
+
+  peerConnection.onTrack = async (evt) => {
+    evt.streans[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track)
+    })
+  }
+
+  //create sdp offer for peer connection
+  let offer = await peerConnection.createOffer();
+
+  //add offer to peer connection
+  await peerConnection.setLocalDescription(offer)
+
+  //stringify JSON offer object to be parsed
+  document.getElementById('offer-sdp').value = JSON.stringify(offer)
 }
 
 init()
 
+//run createOffer function when create-offer button is clicked
+document.getElementById('create-offer').addEventListener('click',createOffer)
