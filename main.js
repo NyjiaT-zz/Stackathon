1c1c7b54891d10ec2105e67f3d38b7e65e204f5e
NyjiaT <100991776+NyjiaT@users.noreply.github.com>
2022-06-25 14:49:17 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+let localStream;
+let remoteStream;
+
+let init = async () => {
+  localStream = await navigator.mediaDevices.getUserMedia({audio:false, video:true});
+
+  remoteStream = new MediaStream()
+
+  document.getElementById('user-1').srcObject = localStream
+  document.getElementById('user-2').srcObject = remoteStream
+}
+
+init()
 
